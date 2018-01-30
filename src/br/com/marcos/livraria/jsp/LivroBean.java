57f85f38dc2ad8a0d93eaf54533c649a30aeb0c2
Marcos Rafael <marcosrrj2@gmail.com>
2018-01-30 12:42:00 -0200

    Criando o Projeto

---
+++ src/br/com/marcos/livraria/jsp/LivroBean.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * @author msrj
+ *
+ */
+package br.com.marcos.livraria.jsp;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class LivroBean{
+
+	public void gravar(){
+		System.out.println("Livro Gravado");
+	}
+}
